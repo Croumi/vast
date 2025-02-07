4c4c410e5b0c819e88ea11b4d5f43b4390969509
fj016 <117085317+fj016@users.noreply.github.com>
2025-02-08 00:33:56 +0100

    Update parsePythonWl.py

--- parsePythonWl.py
+++ parsePythonWl.py
@@ -9,30 +9,42 @@ def decode_unicode_if_necessary(string):
 file=sys.argv[1]
 mode=sys.argv[2]
 
-# Mode 1 = Numbers
-# Mode 2 = Cut and capitalize ("HELLO WORLD" > "HelloWorld")
-year_seperators = ['', '_', '-', '@']
+
 
 
 
 if file and mode == '1':
+    print(f"Processing {file}...")
     with open(file, 'r') as file:
         for line in file:
-            for y in str(range(0, 10000)):
-                for x in year_seperators:
-                    print(f"{line.strip()}{x}{y}")
+            original = line.strip()
 
-if file and mode == '2':
-    with open(file, 'r') as file:
-        for line in file:
-            line = line.strip()
-            decode_unicode_if_necessary(line)
-            line_without_space = "".join([word for word in line.title().split(" ")])
-            print(f'{line_without_space}')
-            decode_unicode_if_necessary(line_without_space)
-            if '-' in line:
-                dash_split = "".join([word for word in line_without_space.split("-")])
-                print(f'{dash_split}')
+            no_underscores = original.replace('_', ' ')
+            no_underscores_collapsed = original.replace('_', '')
+            slash_to_newline = original.replace('/', '\n')
+            slash_to_space = original.replace('/', ' ')
+            line_without_space = "".join([word for word in original.title().split(" ")])
+            dash_split = "".join([word for word in line_without_space.split("-")])
+
+            decode_unicode_if_necessary(original)
+            print(original)
+            if no_underscores != original:
+                decode_unicode_if_necessary(no_underscores)
+                print(no_underscores)
+            if no_underscores_collapsed != original:
+                decode_unicode_if_necessary(no_underscores_collapsed)
+                print(no_underscores_collapsed)
+            if slash_to_newline != original:
+                print(slash_to_newline)
+                decode_unicode_if_necessary(slash_to_newline)
+            if slash_to_space != original:
+                print(slash_to_space)
+                decode_unicode_if_necessary(slash_to_space)
+            if line_without_space != original:
+                print(line_without_space)
+                decode_unicode_if_necessary(line_without_space)
+            if dash_split != original:
+                print(dash_split)
                 decode_unicode_if_necessary(dash_split)
+    print("Done !")        
             
-
